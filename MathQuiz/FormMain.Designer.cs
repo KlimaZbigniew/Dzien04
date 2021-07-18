2cc2cec4e88854f5409b0ca15a6c4c83c683828c
kurs <kurs@DESKTOP-V3HDKPQ>
2021-07-18 16:24:31 +0200

    MathQuiz

---
+++ MathQuiz/FormMain.Designer.cs
@@ -0,0 +1,241 @@
+﻿
+namespace MathQuiz
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblTimer = new System.Windows.Forms.Label();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.lblQ1 = new System.Windows.Forms.Label();
+            this.numQ1 = new System.Windows.Forms.NumericUpDown();
+            this.numQ4 = new System.Windows.Forms.NumericUpDown();
+            this.lblQ4 = new System.Windows.Forms.Label();
+            this.numQ3 = new System.Windows.Forms.NumericUpDown();
+            this.lblQ3 = new System.Windows.Forms.Label();
+            this.numQ2 = new System.Windows.Forms.NumericUpDown();
+            this.lblQ2 = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.timerQuiz = new System.Windows.Forms.Timer(this.components);
+            this.panel1Quiz = new System.Windows.Forms.Panel();
+            this.statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numQ1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numQ4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numQ3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numQ2)).BeginInit();
+            this.panel1Quiz.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblTimer
+            // 
+            this.lblTimer.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblTimer.Location = new System.Drawing.Point(31, 40);
+            this.lblTimer.Name = "lblTimer";
+            this.lblTimer.Size = new System.Drawing.Size(329, 39);
+            this.lblTimer.TabIndex = 0;
+            this.lblTimer.Text = "Pozostały czas: XX sek.";
+            this.lblTimer.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 361);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(503, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(58, 17);
+            this.toolStripStatusLabel1.Text = "wersja 1.0";
+            // 
+            // lblQ1
+            // 
+            this.lblQ1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblQ1.Location = new System.Drawing.Point(51, 104);
+            this.lblQ1.Name = "lblQ1";
+            this.lblQ1.Size = new System.Drawing.Size(174, 40);
+            this.lblQ1.TabIndex = 2;
+            this.lblQ1.Text = "11 +11 =";
+            this.lblQ1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // numQ1
+            // 
+            this.numQ1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.numQ1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.numQ1.InterceptArrowKeys = false;
+            this.numQ1.Location = new System.Drawing.Point(231, 109);
+            this.numQ1.Name = "numQ1";
+            this.numQ1.Size = new System.Drawing.Size(120, 35);
+            this.numQ1.TabIndex = 2;
+            this.numQ1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.numQ1.UpDownAlign = System.Windows.Forms.LeftRightAlignment.Left;
+            this.numQ1.Enter += new System.EventHandler(this.numQ1_Enter);
+            // 
+            // numQ4
+            // 
+            this.numQ4.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.numQ4.InterceptArrowKeys = false;
+            this.numQ4.Location = new System.Drawing.Point(231, 232);
+            this.numQ4.Name = "numQ4";
+            this.numQ4.Size = new System.Drawing.Size(120, 35);
+            this.numQ4.TabIndex = 5;
+            this.numQ4.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // lblQ4
+            // 
+            this.lblQ4.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblQ4.Location = new System.Drawing.Point(51, 227);
+            this.lblQ4.Name = "lblQ4";
+            this.lblQ4.Size = new System.Drawing.Size(174, 40);
+            this.lblQ4.TabIndex = 4;
+            this.lblQ4.Text = "11 / 11 =";
+            this.lblQ4.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // numQ3
+            // 
+            this.numQ3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.numQ3.InterceptArrowKeys = false;
+            this.numQ3.Location = new System.Drawing.Point(231, 191);
+            this.numQ3.Name = "numQ3";
+            this.numQ3.Size = new System.Drawing.Size(120, 35);
+            this.numQ3.TabIndex = 4;
+            this.numQ3.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // lblQ3
+            // 
+            this.lblQ3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblQ3.Location = new System.Drawing.Point(51, 186);
+            this.lblQ3.Name = "lblQ3";
+            this.lblQ3.Size = new System.Drawing.Size(174, 40);
+            this.lblQ3.TabIndex = 6;
+            this.lblQ3.Text = "11 *11 =";
+            this.lblQ3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // numQ2
+            // 
+            this.numQ2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.numQ2.InterceptArrowKeys = false;
+            this.numQ2.Location = new System.Drawing.Point(231, 150);
+            this.numQ2.Name = "numQ2";
+            this.numQ2.Size = new System.Drawing.Size(120, 35);
+            this.numQ2.TabIndex = 3;
+            this.numQ2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // lblQ2
+            // 
+            this.lblQ2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblQ2.Location = new System.Drawing.Point(-5, 64);
+            this.lblQ2.Name = "lblQ2";
+            this.lblQ2.Size = new System.Drawing.Size(174, 40);
+            this.lblQ2.TabIndex = 8;
+            this.lblQ2.Text = "11 -11 =";
+            this.lblQ2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnStart.Location = new System.Drawing.Point(168, 291);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(149, 39);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // timerQuiz
+            // 
+            this.timerQuiz.Interval = 1000;
+            this.timerQuiz.Tick += new System.EventHandler(this.timerQuiz_Tick);
+            // 
+            // panel1Quiz
+            // 
+            this.panel1Quiz.Controls.Add(this.lblQ2);
+            this.panel1Quiz.Location = new System.Drawing.Point(37, 76);
+            this.panel1Quiz.Name = "panel1Quiz";
+            this.panel1Quiz.Size = new System.Drawing.Size(344, 209);
+            this.panel1Quiz.TabIndex = 9;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(503, 383);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.numQ2);
+            this.Controls.Add(this.numQ3);
+            this.Controls.Add(this.lblQ3);
+            this.Controls.Add(this.numQ4);
+            this.Controls.Add(this.lblQ4);
+            this.Controls.Add(this.numQ1);
+            this.Controls.Add(this.lblQ1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.lblTimer);
+            this.Controls.Add(this.panel1Quiz);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "FormMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MathQuiz";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormMain_FormClosing);
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numQ1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numQ4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numQ3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numQ2)).EndInit();
+            this.panel1Quiz.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTimer;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.Label lblQ1;
+        private System.Windows.Forms.NumericUpDown numQ1;
+        private System.Windows.Forms.NumericUpDown numQ4;
+        private System.Windows.Forms.Label lblQ4;
+        private System.Windows.Forms.NumericUpDown numQ3;
+        private System.Windows.Forms.Label lblQ3;
+        private System.Windows.Forms.NumericUpDown numQ2;
+        private System.Windows.Forms.Label lblQ2;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Timer timerQuiz;
+        private System.Windows.Forms.Panel panel1Quiz;
+    }
+}
+
---
+++ MathQuiz/FormMain.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MathQuiz
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            panel1Quiz.BringToFront();
+            SetupQuiz();
+        }
+
+        private Random random = new Random(0);
+
+        private int RandomNumber(int min, int max)
+        {
+            
+            return random.Next(min, max); 
+        }
+        int timeleft = 60; //inicjalna wartosć czasu na rozwiązanie
+
+        Quiz quiz1, quiz2, quiz3, quiz4;
+
+        private void timerQuiz_Tick(object sender, EventArgs e)
+        {
+            timeleft--;
+            lblTimer.Text = string.Format("Pozostały czas: {0} sek.", timeleft);
+            lblTimer.ForeColor = Color.Black;
+
+            if (timeleft == 0)
+            {
+                lblTimer.Text = "Czas minął";
+                timerQuiz.Enabled = false;
+                btnStart.Text = "START";
+                CheckAnswers();
+            }
+            else if (timeleft <= 15)
+                lblTimer.ForeColor = Color.Red;
+            else if (timeleft <= 30)
+                lblTimer.ForeColor = Color.Yellow;
+            else
+                lblTimer.ForeColor = Color.Black;
+        }
+
+        private void numQ1_Enter(object sender, EventArgs e)
+        {
+           // numQ1.SelectAll();
+        }
+
+        private void FormMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            string msg = "Czy na pewno zamknąc aplikację?";
+            string title = "Pytanie";
+            DialogResult result = MessageBox.Show(msg, title, MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.No)
+            {
+                e.Cancel = true;
+            }
+        }
+
+        private void CheckAnswers()
+        {
+            int correctAnswers = 0;
+            if (numQ1.Value == quiz1.Result) correctAnswers++;
+            if (numQ2.Value == quiz2.Result) correctAnswers++;
+            if (numQ3.Value == quiz3.Result) correctAnswers++;
+            if (numQ4.Value == quiz4.Result) correctAnswers++;
+
+            MessageBox.Show(string.Format("Liczba poprawnych odpowiedzi:\n{0}/4", correctAnswers));
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (timerQuiz.Enabled)
+            {
+                //zakoncz quis
+                timerQuiz.Enabled = false;
+                btnStart.Text = "START";
+                CheckAnswers();
+            }
+            else
+            {
+                //uruchom quiz
+                SetupQuiz();
+                panel1Quiz.Visible = false;
+                timerQuiz.Enabled = true;
+                btnStart.Text = "STOP";
+            }
+        }
+
+        private void SetupQuiz()
+        {
+            numQ1.Controls[0].Enabled = false;
+            numQ1.Controls[0].Width = 0;
+            numQ1.Controls[0].Visible = false;
+            //numQ1.Controls[0].ControlRemoved;
+
+            //zerowanie odpowiedzi i ustawienia min.max w numericUpDowm
+            numQ1.Value = 0; numQ1.Minimum = Int32.MinValue; numQ1.Maximum = Int32.MaxValue;
+            numQ2.Value = 0; numQ2.Minimum = Int32.MinValue; numQ2.Maximum = Int32.MaxValue;
+            numQ3.Value = 0; numQ3.Minimum = Int32.MinValue; numQ3.Maximum = Int32.MaxValue;
+            numQ4.Value = 0; numQ4.Minimum = Int32.MinValue; numQ4.Maximum = Int32.MaxValue;
+
+            quiz1 = new Quiz(RandomNumber(-10, -1), RandomNumber(2, 20), "+");
+            quiz2 = new Quiz(RandomNumber(-10, -1), RandomNumber(2, 20), "-");
+            quiz3 = new Quiz(RandomNumber(-10, -1), RandomNumber(2, 20), "*");
+            quiz4 = new Quiz(RandomNumber(-10, -1), RandomNumber(2, 20), "/");
+            //MessageBox.Show(q.ToString());
+            lblQ1.Text = quiz1.ToString();
+            lblQ2.Text = quiz2.ToString();
+            lblQ3.Text = quiz3.ToString();
+            lblQ4.Text = quiz4.ToString();
+
+            timeleft = 60;
+            lblTimer.Text = string.Format("Pozostały czas: {0} sek.", timeleft);
+            lblTimer.ForeColor = Color.Black;
+
+            btnStart.Text = "START";
+        }
+    }
+}
